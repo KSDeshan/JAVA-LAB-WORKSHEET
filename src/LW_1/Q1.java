22220acdd3d28770734718e6aafaebe09fd6ea29
Sandun Deshan <deshansandun961@gmail.com>
2025-09-12 09:03:40 +0530

    q1 code

--- src/LW_1/Q1.java
+++ src/LW_1/Q1.java
@@ -1,5 +1,23 @@
 package LW_1;
 
-public class Q1 {
+import java.util.Scanner;
 
+public class Q1 {
+   public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+
+       System.out.print("Enter odd length word:");
+       String word = sc.nextLine();
+
+       int length = word.length();
+
+       if (length % 2 ==0){
+          System.out.println("Error: Please enter a word with odd number of characters.");
+       }else {
+           int middleIndex = length /2;
+           char middleChar = word.charAt(middleIndex);
+           System.out.println("Middle character:" + middleChar);
+       }
+       sc.close();
+   }
 }
