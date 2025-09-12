ae4ef5c40dd0c2f94efcae77632c972e06f7e9a1
Sandun Deshan <deshansandun961@gmail.com>
2025-09-12 20:35:38 +0530

    q4 code

---
+++ src/LW_1/Q4.java
@@ -0,0 +1,19 @@
+package LW_1;
+
+import javax.swing.*;
+
+public class Q4 {
+    public static void main(String[] args){
+        JFrame frame;
+        frame = new JFrame();
+        frame.setSize(300, 200);
+
+        frame.setTitle("mY first frame");
+
+        frame.setLocation(100, 50);
+
+        frame.setVisible(true);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
