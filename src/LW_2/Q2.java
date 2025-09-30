71b49e6961b6c5ea433bf826da9cc718d46befad
Sandun Deshan <deshansandun961@gmail.com>
2025-09-30 17:47:09 +0530

    Q2 answer

--- src/LW_2/Q2.java
+++ src/LW_2/Q2.java
@@ -1,4 +1,37 @@
 package LW_2;
-
+import java.util.Scanner;
 public class Q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--**Program to count digits of numbers**--");
+
+
+        while (true) {
+            System.out.print("Enter an integer: ");
+            int num = sc.nextInt();
+
+            if (num < 0) {     // stop if negative
+                System.out.println("Program ended!");
+                break;
+            }
+
+            int count = 0;
+
+            if (num == 0) {
+                count = 1;
+            } else {
+                if (num < 0) {
+                    num = -num;
+                }
+                while (num > 0) {
+                    num = num / 10;
+                    count++;
+                }
+            }
+
+            System.out.println("Number of digits = " + count);
+        }
+
+    }
 }
