cced41f64918c4e5336d6b55ee20371ed1e619fb
Sandun Deshan <deshansandun961@gmail.com>
2025-10-03 16:01:41 +0530

    Q5 create and answer

---
+++ src/LW_2/Q5.java
@@ -0,0 +1,26 @@
+package LW_2;
+
+import java.util.Scanner;
+public class Q5 {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a sentence:");
+            String sentence = sc.nextLine();
+
+
+            String cleaned = sentence.replaceAll("[^a-zA-Z]", "").toLowerCase();
+
+
+            String reversed = new StringBuilder(cleaned).reverse().toString();
+
+            if (cleaned.equals(reversed)) {
+                System.out.println("The sentence is a palindrome.");
+            } else {
+                System.out.println("The sentence is not a palindrome.");
+            }
+
+        }
+    }
+
+
