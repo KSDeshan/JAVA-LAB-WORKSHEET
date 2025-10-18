91cfbc2b73c22d2cff027eaeec99d49ed1b43811
Sandun Deshan <deshansandun961@gmail.com>
2025-10-18 12:27:29 +0530

    LW3 AND Q1 create and answers

---
+++ src/LW_3/Q1.java
@@ -0,0 +1,46 @@
+package LW_3;
+
+import java.util.Scanner;
+class Temperature {
+    private double celsius;
+
+
+    public Temperature() {
+        celsius = 0.0;
+    }
+    public Temperature(double celsius) {
+        this.celsius = celsius;
+    }
+    public double toCelsius() {
+        return celsius;
+    }
+    public double toFahrenheit() {
+        return celsius * 9 / 5 + 32;
+    }
+
+    public void setCelsius(double celsius) {
+        this.celsius = celsius;
+    }
+
+
+    public void setFahrenheit(double fahrenheit) {
+        this.celsius = (fahrenheit - 32) * 5 / 9;
+    }
+}
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Enter temperature in Celsius: ");
+        double inputCelsius = scanner.nextDouble();
+
+
+        Temperature temp = new Temperature(inputCelsius);
+        System.out.println("Equivalent temperature in Fahrenheit: " + temp.toFahrenheit());
+
+        scanner.close();
+    }
+}
+
