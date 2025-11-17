b363d5f9a34a5e84bd2dcfa19c75582bcce95dea
Sandun Deshan <deshansandun961@gmail.com>
2025-11-17 09:11:33 +0530

    Q1 answer

--- src/LW_4/Q1.java
+++ src/LW_4/Q1.java
@@ -1,4 +1,33 @@
 package LW_4;
 
+import java.util.Scanner;
+
 public class Q1 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = input.nextInt();
+
+        System.out.print("Enter third number: ");
+        int c = input.nextInt();
+
+        int smallest;
+
+        if (a <= b && a <= c) {
+            smallest = a;
+        } else if (b <= a && b <= c) {
+            smallest = b;
+        } else {
+            smallest = c;
+        }
+
+        System.out.println("The smallest number is: " + smallest);
+
+        input.close();
+    }
 }
